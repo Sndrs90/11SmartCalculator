33df2ca2f52e0f4d850899cd340322c59e786423
Sanders <sndrs90@gmail.com>
2025-02-10 10:35:20 +0300

    Stage 3 completed

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -8,6 +8,10 @@ fun main() {
                 println("Bye!")
                 break
             }
+            input == "/help" -> {
+                println("The program calculates the sum of numbers")
+                continue
+            }
             input.isEmpty() -> continue
         }
         processInput(input)
@@ -17,7 +21,7 @@ fun main() {
 private fun processInput(input: String) {
     val nums = input.split(" ").mapNotNull { it.toIntOrNull() }
     when (nums.size) {
-        2 -> println(nums[0] + nums[1])
+        in 2..1000 -> println(nums.sum())
         1 -> println(nums[0])
         else -> println("Not integers")
     }
